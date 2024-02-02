74c1ae3fafa1a58bfa6670d155605c906ba0a8f9
Bhanu <bhanusinghbhadouriya5014@gmail.com>
2024-02-03 01:35:57 +0530

    add dark theme change (context api)

--- 08miniContext/src/Components/Login.jsx
+++ 08miniContext/src/Components/Login.jsx
@@ -1,30 +1,29 @@
 import React, {useState, useContext} from 'react'
 import UserContext from '../context/UserContext'
 
-
 function Login() {
-    const[username, setUsername] = useState('')
-    const[password, setPassword] = useState('')
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
     const {setUser} = useContext(UserContext)
 
     const handleSubmit = (e) => {
-        e.prventDefault()
+        e.preventDefault()
         setUser({username, password})
     }
   return (
     <div>
-        <h2>Login</h2> <br />
-        <input type="text"
+        <h2>Login</h2>
+        <input type='text'
         value={username}
-        onChange={(e)=> setUsername(e.target.value)}
-        placeholder='username' /> <br />
-    
-        <input type="password"
+        onChange={(e) => setUsername(e.target.value) }
+        placeholder='username'  />
+        {" "}
+        <input type='text' 
         value={password}
-        onChange={(e)=> setPassword(e.target.value)}
-        placeholder='password' /> <br />
+        onChange={(e) => setPassword(e.target.value) }
+        placeholder='password'  />
         <button onClick={handleSubmit}>Submit</button>
-
     </div>
   )
 }
